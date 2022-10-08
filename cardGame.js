2ee633ff9e4201cf53a08ca40859e5aa20dcdb64
JakePy <101231593+jakepy@users.noreply.github.com>
2022-10-07 23:44:43 -0700

    Update cardGame.js

--- cardGame.js
+++ cardGame.js
@@ -1,7 +1,5 @@
 const cards = ['diamond', 'spade', 'heart', 'club'];
 
-// Write your code below
-
 let currentCard
 while (currentCard !== 'spade') {
   currentCard = cards[Math.floor(Math.random() * 4)]
